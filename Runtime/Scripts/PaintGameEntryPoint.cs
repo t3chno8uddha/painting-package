4d670015846f1aba0fe4d3b5e9b6257b1aa90b7b
t3chno8uddha <zurachstanima@gmail.com>
2025-09-15 16:53:42 +0400

    commit

--- Runtime/Scripts/PaintGameEntryPoint.cs
+++ Runtime/Scripts/PaintGameEntryPoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PaintGameEntryPoint : BaseMiniGameEntryPoint
 {
+    //
     [SerializeField] private GameObject gamePrefab;
     private IGameOverScreen gameOverScreen = default;
     private GameOverScreenData GameOverData => GetGameOverScreenData;
